14d24d0e3d4ba8c5374bebdf31978ba9dcaf6241
moonsu1 <44462190+moonsu1@users.noreply.github.com>
2023-08-04 06:03:32 +0000

    final update

--- batch_prediction.sh
+++ batch_prediction.sh
@@ -4,4 +4,4 @@ PIPENV_PIPFILE=$ROOT_PATH/Pipfile
 
 export PATH=$PATH:/usr/local/py-utils/bin
 export PIPENV_PIPFILE=$PIPENV_PIPFILE
-pipenv run python $ROOT_PATH/batch_prediction.py >> $ROOT_PATH/cron.log 2>&1cr
+pipenv run python $ROOT_PATH/batch_prediction.py >> $ROOT_PATH/cron.log 2>&1
